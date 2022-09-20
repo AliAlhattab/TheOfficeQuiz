52ab795986db3855d3da253298655d7c113bf1fd
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-09-19 20:56:03 -0400

    completed nav bar mobile and tablet. might add modifer later

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Nav.scss'
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className='nav'>
+        <Link className='nav__options' to='/'><div className='nav__logo'></div></Link>
+        <ul className='nav__list'>
+            <Link className='nav__options' to='/'><li className='nav__options'>Home</li></Link>
+            <Link className='nav__options' to='/leadersboard'><li className='nav__options'>Leaderboard</li></Link>
+        </ul>
+    </nav>
   )
 }
 
