f6dca266312bde8274270ca5a1eabcd3d480bd21
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-09-27 23:19:55 -0400

    axios get request for the quote and character name

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Game.scss'
+import axios from 'axios'
 
-export default function Game() {
+function Game() {
+
+  const [quote, getQuote] = useState('')
+  const [firstName, getFirstName] = useState('')
+  const [lastName, getLastName] = useState('')
+  const [randomFirstName, getRandomFirstName] = useState('')
+  const [randomLastName, getRandomLastName] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('https://www.officeapi.dev/api/quotes/random')
+    .then(response => {
+      getQuote(response.data.data.content)
+      getFirstName(response.data.data.character.firstname)
+      getLastName(response.data.data.character.lastname)
+      console.log(response.data.data)
+    })
+    .catch(err => {
+      console.log('Cannot get quote')
+    });
+
+  }, [])
+
+  useEffect((array) => {
+    axios
+    .get('https://www.officeapi.dev/api/characters/')
+    .then(response => {
+     getRandomFirstName(response.data.data[0].firstName)
+      console.log(response.data.data)
+    })
+    .catch(err => {
+      console.log('Cannot get character')
+    });
+
+  }, [])
+  
+
   return (
-    <div>Game</div>
+    <div>
+      <h1>{quote}</h1>
+      <h2>{firstName} {lastName}</h2>
+      <p>{randomFirstName}</p>
+    </div>
   )
 }
+
+export default Game
