04582007edfe929c2aad51f88cd6c1fd48cff33c
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-09-30 20:30:19 -0400

    game sass complete for all sizes

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './Game.scss'
 import axios from 'axios'
+import logo from '../../assets/images/dunder_mifflin.png'
 
 function Game() {
 
   const [quote, getQuote] = useState('')
   const [name, getName] = useState('')
-  const [randomName, getRandomName] = useState([])
+  const [randomName1, getRandomName1] = useState([])
+  const [randomName2, getRandomName2] = useState([])
+  const [randomName3, getRandomName3] = useState([])
 
   useEffect(() => {
     axios
@@ -21,28 +24,51 @@ function Game() {
 
   }, [])
 
-  useEffect((array) => {
+  useEffect(() => {
     axios
     .get('https://www.officeapi.dev/api/characters/')
     .then(response => {
-     getRandomName(response.data.data[Math.floor(Math.random() * response.data.data.length)].firstname)
+     getRandomName1(response.data.data[Math.floor(Math.random() * response.data.data.length)].firstname)
+     getRandomName2(response.data.data[Math.floor(Math.random() * response.data.data.length)].firstname)
+     getRandomName3(response.data.data[Math.floor(Math.random() * response.data.data.length)].firstname)
+
     })
     .catch(err => {
       console.log('Cannot get character')
     });
 
   }, [])
+
+  useEffect(() => {
+
+
+
+  }, [])
+
   
 
   return (
-    <div>
-      <h1>{quote}</h1>
-      <p>{name}</p>
-      <p>{randomName}</p>
-      <p>{randomName}</p>
-      <p>{randomName}</p>
+    <form className='game'>
+      <div className='game__image'>
+      <img className='game__logo' src={logo} alt='Dunder Mifflin Logo' />
+      </div>
+      <div className='game__instructions'>
+        <p className='game__text'>Who said this quote?</p>
+      </div>
+      <div className='game__question'>
+      <h1 className='game__quote'>{quote}</h1>
+      </div>
+      <div className='game__button'>
+      <button className='game__option'>{name}</button>
+      <button className='game__option'>{randomName1}</button>
+      <button className='game__option'>{randomName2}</button>
+      <button className='game__option'>{randomName3}</button>
+      </div>
+      <div className='game__counter'>
+        <p className='game__score'>Your Score: 0</p>
+      </div>
       
-    </div>
+    </form>
   )
 }
 
