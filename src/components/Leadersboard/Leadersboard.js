235b9e78bac9d011a47145e0a2bc98e7c7ae113a
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-10-15 06:03:17 -0400

    leaderboard axios get

--- src/components/Leadersboard/Leadersboard.js
+++ src/components/Leadersboard/Leadersboard.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import './Leadersboard.scss'
 
 function Leadersboard() {
 
+    const [leaderboard, getLeaderboard] = useState('')
+    
+    useEffect(() => {
+        axios
+        .get('http://localhost:8080/players')
+        .then(response => {
+            getLeaderboard(response.data)
+            console.log(response.data)
+        }, [])
+
+    })
 
   return (
     <section className='leadersboard'>
@@ -16,13 +29,19 @@ function Leadersboard() {
                 <li className='leadersboard__section'>Score</li>
             </ul>
         </div>
-        <div className='leadersboard__players'>
-            <ul className='leadersboard__list'>
-                <li className='leadersboard__rank'>1</li>
-                <li className='leadersboard__rank'>Ali</li>
-                <li className='leadersboard__rank'>10</li>
-            </ul>
-        </div>
+        { leaderboard?.map((data) =>{
+            return(
+                <div className='leadersboard__players'>
+                <ul className='leadersboard__list'>
+                    <li className='leadersboard__rank'>1</li>
+                    <li className='leadersboard__rank'>{data.name}</li>
+                    <li className='leadersboard__rank'>{data.score}</li>
+                </ul>
+            </div>
+
+            )
+        })
+}
     </section>
   )
 }
